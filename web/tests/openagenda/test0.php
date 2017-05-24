13797013717d8480664dd091a12a18f142224e42
orl-patrick <patrickfaucheux@alicepro.fr>
2017-05-24 15:35:02 +0200

    autres tests

--- web/tests/openagenda/test0.php
+++ web/tests/openagenda/test0.php
@@ -6,8 +6,10 @@
 
 $publicKey = '1F6bZ8wy4X1IU7Myf7lJxcwwE5UdnBFo';
 
+
 //$data = json_decode(file_get_contents("https://api.openagenda.com/v1/agendas?key=$publicKey"));
 
+/*
 // --- recuperation UID agenda
 $nom = 'duri-wild';
 //$nom = 'paloaltours';
@@ -15,19 +17,23 @@ $data = json_decode(file_get_contents("https://api.openagenda.com/v1/agendas/uid
 $uid = 'Non trouvé';
 if (true === $data->success) $uid = $data->data->uid;
 echo $uid;
+*/
 
-
+/*
 // --- recuperation des evenements de l'agenda
 $data = json_decode(file_get_contents("https://api.openagenda.com/v1/agendas/$uid/events?key=$publicKey"));
 if (true === $data->success) $infos = $data->data;
 var_dump($data);
-
+*/
 
 
 // --- recuperation des lieux
-$e_uid = '97185906';
-$l_uid = '46297259';
-$url = "https://api.openagenda.com/v1/events/$e_uid/locations?key=$publicKey";
+$a_uid = '91057368'; $slug='duri-wild'; // agenda
+
+$e_uid = '44566425 '; // event
+$l_uid = '2859504 '; // location
+$url = "https://api.openagenda.com/v1/agendas/$a_uid/events?key=$publicKey";
+echo "<p>$url</p>";
 $data = json_decode(file_get_contents($url));
 if (true === $data->success) $infos = $data->data;
 var_dump($data);
