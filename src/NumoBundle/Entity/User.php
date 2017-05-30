5ee3084977ac84f1a4cd9a1aea1a324b45eff4fa
Fanny-Jan <fanny.jan12@gmail.com>
2017-05-30 17:50:32 +0200

    overwrite fosuser. manque adherent

--- src/NumoBundle/Entity/User.php
+++ src/NumoBundle/Entity/User.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace AppBundle\Entity;
+namespace NumoBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -17,6 +18,147 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre nom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Le nom est trop court.",
+     *     maxMessage="Le nom est trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre prénom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Le prénom est trop court.",
+     *     maxMessage="Le prénom est trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $firstname;
+
+    /**
+     * @ORM\Column(type="text", length=653, nullable=true)
+     *
+     * @Assert\Length(
+     *     max=653,
+     *     maxMessage="Le message est trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Length(
+     *     max=255,
+     *     maxMessage="L'url est trop longue.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $imageUrl;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     * @Assert\NotBlank(groups={"Registration", "Profile"})
+     */
+    protected $trust;
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param mixed $imageUrl
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrust()
+    {
+        return $this->trust;
+    }
+
+    /**
+     * @param mixed $trust
+     */
+    public function setTrust($trust)
+    {
+        $this->trust = $trust;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+
 
     public function __construct()
     {
