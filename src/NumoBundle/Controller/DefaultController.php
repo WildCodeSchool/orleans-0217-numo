843496f737998a67539accde4c05bcb97e3c0977
hudavid1995 <hudavidfr@gmail.com>
2017-06-02 16:33:15 +0200

    index route

--- src/NumoBundle/Controller/DefaultController.php
+++ src/NumoBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/index")
+     * @Route("/")
      */
     public function indexAction()
     {
