a5c7fedf202a95d583364d690cc2eacb4843cf29
wilder9 <sarah0peltier@gmail.com>
2017-06-02 11:38:19 +0200

    création admin sans easy admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,8 +18,6 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new NumoBundle\NumoBundle(),
-            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            new Vich\UploaderBundle\VichUploaderBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'tests'], true)) {
