81d146ca68171618e7981fbc34ebf018d91ca3ba
orl-patrick <patrickfaucheux@alicepro.fr>
2017-06-14 17:41:04 +0200

    correction conflit page new event

--- src/NumoBundle/Entity/Category.php
+++ src/NumoBundle/Entity/Category.php
@@ -24,7 +24,7 @@ class Category
     /**
      * @var string
      *
-     * @ORM\Column(name="category", type="string", length=255, unique=true)
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
      */
     private $name;
 
