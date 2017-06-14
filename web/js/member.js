015d64af0e38bdc8655a7416099f336e3bd4df60
hudavid1995 <hudavidfr@gmail.com>
2017-06-14 16:49:48 +0200

    delete js script useless

--- web/js/member.js
+++ web/js/member.js
@@ -43,7 +43,7 @@ $('.AlphabetNav a').click(function(evt){
                 $contactName = $contact.find('.memberName'),
                 $nameArr = $contactName.text().split(' ');
 
-            console.log($nameArr[0].split('')[0].toLowerCase());
+            //console.log($nameArr[0].split('')[0].toLowerCase());
 
             if ($nameArr[0].split('')[0].toLowerCase() !== $navItem.text().toLowerCase()) {
                 $contact.hide();
@@ -53,12 +53,5 @@ $('.AlphabetNav a').click(function(evt){
 });
 
 
-$('.AlphabetNav a').click(function(evt) {
-    evt.preventDefault();
 
-    if ($navItem.hasClass('active')) {
-        $navItem.removeClass('active');
-
-    }
-});
 
