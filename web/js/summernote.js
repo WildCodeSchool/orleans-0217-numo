c34d737bd73dff8b5e71f9c99bf9c175d208533a
wilder9 <sarah0peltier@gmail.com>
2017-06-27 17:50:39 +0200

    corrections

--- web/js/summernote.js
+++ web/js/summernote.js
@@ -2,9 +2,11 @@
  * Created by wilder9 on 13/06/17.
  */
 $(document).ready(function() {
-    $('#numobundle_pagecontent_content').summernote({
+    $('#company_presentationContent').summernote({
+/*
+        minHeight: 300,
+*/
         toolbar: [
-            // [groupName, [list of button]]
             ['style', ['bold', 'italic', 'underline']],
             ['fontsize', ['fontsize']],
             ['color', ['color']],
