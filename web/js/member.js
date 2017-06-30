f38047d588f8f19fca61f43956fe91b69498ae65
Fanny-Jan <fanny.jan12@gmail.com>
2017-06-30 11:07:13 +0200

    tests

--- web/js/member.js
+++ web/js/member.js
@@ -21,14 +21,29 @@ $(document).ready(function(){
 
 });
 
+// $('#AlphabetNavAll').click(function(evt) {
+//     evt.preventDefault();
+//
+//     var $navItem = $(this),
+//         $contacts = $('.showMember');
+//
+//     $navItem.removeClass('active');
+//     $contacts.show();
+// });
+
 
 $('.AlphabetNav a').click(function(evt){
     evt.preventDefault();
 
     var $navItem = $(this),
-        $contacts = $('.showMember');
+        $contacts = $('.showMember'),
+        $all = $('#AlphabetNavAll');
 
     $contacts.show();
+
+    if ($all.hasClass('active')) {
+        $contacts.show();
+    }
 
     if ($navItem.hasClass('active')) {
         $navItem.removeClass('active');
@@ -41,8 +56,6 @@ $('.AlphabetNav a').click(function(evt){
             var $contact = $(contact),
                 $contactName = $contact.find('.memberName'),
                 $nameArr = $contactName.text().split(' ');
-
-            //console.log($nameArr[0].split('')[0].toLowerCase());
 
             if ($nameArr[0].split('')[0].toLowerCase() !== $navItem.text().toLowerCase()) {
                 $contact.hide();
