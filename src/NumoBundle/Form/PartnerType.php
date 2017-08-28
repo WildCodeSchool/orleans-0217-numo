88cf791ff135b5cc95c91180f2eb2b2cb252a454
Fanny-Jan <fanny.jan12@gmail.com>
2017-08-28 14:21:11 +0200

    dernieres modifs

--- src/NumoBundle/Form/PartnerType.php
+++ src/NumoBundle/Form/PartnerType.php
@@ -4,6 +4,8 @@ namespace NumoBundle\Form;
 
 use NumoBundle\Entity\Partner;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -20,7 +22,13 @@ class PartnerType extends AbstractType
             ->add('name')
             ->add('webUrl')
             ->add('imageUrl', FileType::class, ['label' => 'ImageUrl(Image file)'])
-            ->add('active');
+            ->add('active', ChoiceType::class, [
+                'label' => 'Actif',
+                'choices' => array('oui' => 1, 'non' => 0),
+                'expanded' => true,
+                'multiple' => false,
+                'required' => true
+            ]);
     }
     
     /**
